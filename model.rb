aa1a2fdd7df1e7c0167071fec70a3a6f571d2fe2
Haniel <alu0100721690@ull.edu.es>
2014-11-02 18:30:11 +0000

    Añadiendo contador visitas

--- model.rb
+++ model.rb
@@ -9,6 +9,8 @@ class ShortenedUrl
 	  property :url, Text
 	  property :opc_url, Text
 	  property :email, Text
+    property :created_at, DateTime
+    property :n_visits, Integer
 
 	  has n, :visits
 end
@@ -20,26 +22,10 @@ class Visit
   property  :created_at,  DateTime
   property  :ip,          IPAddress
   property  :country,     String
-  belongs_to  :shortened_url
 
+  #belongs_to :shortened_url
 
-  after :create, :set_country 
-
-  #def set_country
-  #  xml = RestClient.get "http://api.hostip.info/get_xml.php?ip=#{ip}"
-  #  self.country = XmlSimple.xml_in(xml.to_s)
-  #  self.save
-  #end
-
-  #def get_remote_ip(env)
-  #puts "request.url = #{request.url}"
-  #puts "request.ip = #{request.ip}"
-  #if addr = env['HTTP_X_FORWARDED_FOR']
-  #  puts "env['HTTP_X_FORWARDED_FOR'] = #{addr}"
-  #  addr.split(',').first.strip
-  #else
-  #  puts "env['REMOTE_ADDR'] = #{env['REMOTE_ADDR']}"
-  #  env['REMOTE_ADDR']
-  #end
+  #def prueba
+  #  puts "prueba"
 	#end 
 end
