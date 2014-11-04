aea049160482c5659c841733359139b99b69b3fe
Karen Mercedes <alu0100402001@ull.edu.es>
2014-11-04 12:44:35 +0000

    estableciendo pais por ip

--- model.rb
+++ model.rb
@@ -24,4 +24,13 @@ class Visit
   property  :country,     String
 
   belongs_to :shorturl
+
+  before :create, :set_country
+
+  def set_country
+    xml = RestClient.get "http://freegeoip.net/xml/#{ip}"
+    self.country = XmlSimple.xml_in(xml.to_s, { 'ForceArray' => false })['CountryName'].to_s
+    self.save
+  end
+
 end
